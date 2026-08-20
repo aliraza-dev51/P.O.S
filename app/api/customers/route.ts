49068351d4ba662008232ec6a974116aa5eeb057
POS Developer <dev@pos-system.local>
2026-08-20 22:16:01 +0500

    remove old prisma modules

--- app/api/customers/route.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "@/lib/prisma";
-import { NextResponse } from "next/server";
-
-export async function GET(request: Request) {
-    const customers = await prisma.customer.findMany({
-    });
-
-    return NextResponse.json(customers);
-}
